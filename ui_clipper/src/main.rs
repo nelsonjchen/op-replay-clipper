71f82d88aa17631a7fa2ea88e620e118d8aefa21
Nelson Chen <crazysim@gmail.com>
2022-08-23 06:51:25 +0000

    WIP handle tmux

--- ui_clipper/src/main.rs
+++ ui_clipper/src/main.rs
@@ -1,6 +1,7 @@
-use std::{thread, process};
+use std::{process, thread, time::Duration};
 
-use log::{info, error};
+use log::{error, info};
+use tmux_interface::TmuxCommand;
 mod xtigervnc;
 
 fn main() {
@@ -11,12 +12,22 @@ fn main() {
     let _handle = thread::spawn(|| {
         let mut child = xtigervnc::launch_tiger_vnc();
         let status = child.0.wait().expect("failed to wait for child process");
-        error!("Child process exited with status, aborting whole process: {}", status);
+        error!(
+            "Child process exited with status, aborting whole process: {}",
+            status
+        );
         process::exit(1);
     });
 
-    thread::sleep(std::time::Duration::from_secs(30));
+    let mut tmux = TmuxCommand::new();
+    tmux.new_session()
+        .detached()
+        .session_name("repeat")
+        .output()
+        .unwrap();
+    tmux.send_keys().key("sleep 300").key("C-m").output();
+    thread::sleep(Duration::from_secs(300));
+
     // If this thread exits, the whole program exits
     info!("Main thread finished");
 }
-
