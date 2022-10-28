b6c6ba337ddedb4ba4bf7a222c27235a3125bde6
Nelson Chen <crazysim@gmail.com>
2022-10-27 22:51:17 -0700

    Lower smear amount and make it an option

--- clip.sh
+++ clip.sh
@@ -7,6 +7,7 @@
 # ARG_OPTIONAL_SINGLE([target-mb],[m],[Target converted file size in MB],[8])
 # ARG_OPTIONAL_BOOLEAN([e2e-long],[e],[Turn on or off e2e long],[off])
 # ARG_OPTIONAL_SINGLE([jwt-token],[j],[JWT Auth token to use (get token from https://jwt.comma.ai)])
+# ARG_OPTIONAL_SINGLE([smear-amount],[],[Amount of seconds to smear the clip start by before recording starts],[5])
 # ARG_OPTIONAL_BOOLEAN([slow-cpu],[],[Turn on or off slower CPU mode at 0.2x for ~4 core CPUs],[off])
 # ARG_OPTIONAL_SINGLE([video-cwd],[c],[video working and output directory],[/shared])
 # ARG_OPTIONAL_SINGLE([output],[o],[output clip name],[clip.mp4])
@@ -44,6 +45,7 @@ _arg_length_seconds="30"
 _arg_target_mb="8"
 _arg_e2e_long="off"
 _arg_jwt_token=
+_arg_smear_amount="5"
 _arg_slow_cpu="off"
 _arg_video_cwd="/shared"
 _arg_output="clip.mp4"
@@ -52,14 +54,15 @@ _arg_output="clip.mp4"
 print_help()
 {
 	printf '%s\n' "See README at https://github.com/nelsonjchen/op-replay-clipper/"
-	printf 'Usage: %s [-s|--start-seconds <arg>] [-l|--length-seconds <arg>] [-m|--target-mb <arg>] [-e|--(no-)e2e-long] [-j|--jwt-token <arg>] [--(no-)slow-cpu] [-c|--video-cwd <arg>] [-o|--output <arg>] [-h|--help] <route_id>\n' "$0"
+	printf 'Usage: %s [-s|--start-seconds <arg>] [-l|--length-seconds <arg>] [-m|--target-mb <arg>] [-e|--(no-)e2e-long] [-j|--jwt-token <arg>] [--smear-amount <arg>] [--(no-)slow-cpu] [-c|--video-cwd <arg>] [-o|--output <arg>] [-h|--help] <route_id>\n' "$0"
 	printf '\t%s\n' "<route_id>: comma connect route id, segment id is ignored (hint, put this in quotes otherwise your shell might misinterpret the pipe) "
 	printf '\t%s\n' "-s, --start-seconds: Seconds to start at (default: '60')"
 	printf '\t%s\n' "-l, --length-seconds: Clip length (default: '30')"
 	printf '\t%s\n' "-m, --target-mb: Target converted file size in MB (default: '8')"
 	printf '\t%s\n' "-e, --e2e-long, --no-e2e-long: Turn on or off e2e long (off by default)"
 	printf '\t%s\n' "-j, --jwt-token: JWT Auth token to use (get token from https://jwt.comma.ai) (no default)"
-	printf '\t%s\n' "--slow-cpu, --no-slow-cpu: Turn on or off slower CPU mode at 0.25x for ~4 core CPUs (off by default)"
+	printf '\t%s\n' "--smear-amount: Amount of seconds to smear the clip start by before recording starts (default: '5')"
+	printf '\t%s\n' "--slow-cpu, --no-slow-cpu: Turn on or off slower CPU mode at 0.2x for ~4 core CPUs (off by default)"
 	printf '\t%s\n' "-c, --video-cwd: video working and output directory (default: '/shared')"
 	printf '\t%s\n' "-o, --output: output clip name (default: 'clip.mp4')"
 	printf '\t%s\n' "-h, --help: Prints help"
@@ -128,6 +131,14 @@ parse_commandline()
 				;;
 			-j*)
 				_arg_jwt_token="${_key##-j}"
+				;;
+			--smear-amount)
+				test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
+				_arg_smear_amount="$2"
+				shift
+				;;
+			--smear-amount=*)
+				_arg_smear_amount="${_key##--smear-amount=}"
 				;;
 			--no-slow-cpu|--slow-cpu)
 				_arg_slow_cpu="on"
@@ -227,8 +238,7 @@ cleanup
 
 STARTING_SEC=$_arg_start_seconds
 # Sometimes it takes a bit of time for openpilot drawing to settle in.
-# Calculate optimal smear seconds as starting seconds mod 60 plus 30 seconds.
-SMEAR_AMOUNT=$((($STARTING_SEC % 60) + 30))
+SMEAR_AMOUNT=$_arg_smear_amount
 SMEARED_STARTING_SEC=$(($STARTING_SEC - $SMEAR_AMOUNT))
 # SMEARED_STARTING_SEC must be greater than 0
 if [ $SMEARED_STARTING_SEC -lt 0 ]; then
