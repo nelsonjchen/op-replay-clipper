e07c33cbed51d2dadf46bb87472c672907b27bfb
Nelson Chen <crazysim@gmail.com>
2022-11-20 09:55:15 +0000

    more words

--- scratch_run.sh
+++ scratch_run.sh
@@ -11,18 +11,20 @@
 # Terminal not shown in Codespaces? Click the three bars on the top left corner and select Terminal.
 
 /bin/bash clip.sh \
-  `# Get the route / segment id from https://connect.comma.ai` \
-  `# Eg. fe18f736cb0d7813|2022-11-11--20-20-49` \
+  `# Get the route / segment id from https://connect.comma.ai and put it below` \
   "fffffsomedongleidzzzzz|2022-11-11--20-20-49" \
   `# Get a token from https://jwt.comma.ai` \
   --jwt-token "replace_this_with_that_token" \
-  `# Segment IDs start from 0. Multiply the segment number by 60.` \
+  `# Segment IDs start from 0. e.g. fe18f736cb0d7813|2022-11-11--20-20-49-1 has a segment id of 1` \
+  `# Multiply the segment number by 60 and add an offset if you want.` \
   --start-seconds 60 \
   `# It's the length of a clip. Keep it short. Or long, if you want.` \
-  `# Longer clips take much longer though.` \
+  `# Longer clips take proportionally longer to render.` \
   --length-seconds 60 \
-  `# Visit and allow notifications from https://ntfy.sh/ntfy_topic_of_your_choice` \
-  `# so you know when its done and get a nice notification` \
+  `# https://ntfy.sh can be used to provide desktop notifications when a rendering is complete.` \
+  `# Pick a unique topic name of your choice, and replace "ntfy_topic_of_your_choice" below with it` \
+  `# Then, visit and allow desktop notifications from https://ntfy.sh/ntfy_topic_of_your_choice` \
+  `# so you know when its done and get a nice notification on your desktop to come back.` \
   --ntfysh ntfy_topic_of_your_choice \
-  `# Change the output clip file name here` \
+  `# Change the output clip file name here. Any existing files will be overwritten.` \
   --output clip.mp4 
