325fc715964c650f0e5330850ebe9ba92f67e3a2
Nelson Chen <crazysim@gmail.com>
2023-09-04 04:17:34 -0700

    Share openpilot build setup between cog and devcontainer

--- cog/generate.sh
+++ cog/generate.sh
@@ -4,4 +4,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Encode the setup.sh script into cog.template.yaml as base64 of ENCODED_SCRIPT and put it one directory up as cog.yml
-sed -e "s/ENCODED_SCRIPT/$(base64 -w 0 "$DIR"/setup.sh)/" "$DIR"/cog.template.yaml > "$DIR"/../cog.yaml
+sed -e "s/ENCODED_SCRIPT/$(base64 -w 0 "$DIR"/../common/setup.sh)/" "$DIR"/cog.template.yaml > "$DIR"/../cog.yaml
