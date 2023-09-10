86afb4a449985b153ead117be228cd1c4a352d83
Nelson Chen <crazysim@gmail.com>
2023-09-10 05:24:11 +0000

    Cut segment size down on build

--- common/setup.sh
+++ common/setup.sh
@@ -20,6 +20,9 @@ apt-get update -y && apt-get install -y \
     sudo \
     git
 
+# Blow away existing openpilot folder if it exists
+rm -rf /home/batman/openpilot || true
+
 git clone --depth 1 --recurse-submodules https://github.com/commaai/openpilot /home/batman/openpilot
 
 cd /home/batman/openpilot || exit
@@ -44,6 +47,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Replicate.com unfortunately has a very small /dev/shm, so we need to use /var/tmp instead
 find . -type f -exec sed -i 's/\/dev\/shm/\/var\/tmp/g' {} \;
+
+# Replace default segment size to a smaller size 
+find . -type f -exec sed -i 's/#define DEFAULT_SEGMENT_SIZE (10 \* 1024 \* 1024)/#define DEFAULT_SEGMENT_SIZE (3 \* 1024 \* 1024)/g' {} \;
 
 # Replace "constexpr int MIN_SEGMENTS_CACHE = 5;" with "constexpr int MIN_SEGMENTS_CACHE = 2;"
 # in tools/replay/replay.h as for some reason the argument does not appear to be working
