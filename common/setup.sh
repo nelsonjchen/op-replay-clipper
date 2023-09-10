55169da8d5080b2fe6a94a2d198886a5235c9e22
Nelson Chen <crazysim@gmail.com>
2023-09-10 00:15:45 -0700

    Need to increase segment cache to prevent a stutter

--- common/setup.sh
+++ common/setup.sh
@@ -48,12 +48,12 @@ export DEBIAN_FRONTEND=noninteractive
 # Replicate.com unfortunately has a very small /dev/shm, so we need to use /var/tmp instead
 find . -type f -exec sed -i 's/\/dev\/shm/\/var\/tmp/g' {} \;
 
-# Replace default segment size to a smaller size 
+# Replace default segment size to a smaller size
 find . -type f -exec sed -i 's/#define DEFAULT_SEGMENT_SIZE (10 \* 1024 \* 1024)/#define DEFAULT_SEGMENT_SIZE (3 \* 1024 \* 1024)/g' {} \;
 
-# Replace "constexpr int MIN_SEGMENTS_CACHE = 5;" with "constexpr int MIN_SEGMENTS_CACHE = 2;"
+# Replace "constexpr int MIN_SEGMENTS_CACHE = 5;" smaller amount
 # in tools/replay/replay.h as for some reason the argument does not appear to be working
-sed -i 's/constexpr int MIN_SEGMENTS_CACHE = 5;/constexpr int MIN_SEGMENTS_CACHE = 2;/g' tools/replay/replay.h
+sed -i 's/constexpr int MIN_SEGMENTS_CACHE = 5;/constexpr int MIN_SEGMENTS_CACHE = 3;/g' tools/replay/replay.h
 
 scons -j8 tools/replay/replay selfdrive/ui/_ui
 
