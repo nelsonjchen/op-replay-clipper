7d2a1eb883e2f1bcade7148d4fd319d5b27c3db0
Nelson Chen <crazysim@gmail.com>
2024-03-11 05:41:31 +0000

    Remove /dev/shm hack

--- common/setup.sh
+++ common/setup.sh
@@ -54,12 +54,6 @@ export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
     rm -rf /var/lib/apt/lists/* && \
     rm -rf /tmp/*
 
-# Replicate.com unfortunately has a very small /dev/shm, so we need to use /var/tmp instead
-find . -type f -exec sed -i 's/\/dev\/shm/\/var\/tmp/g' {} \;
-
-# Replace default segment size to a smaller size
-find . -type f -exec sed -i 's/#define DEFAULT_SEGMENT_SIZE (10 \* 1024 \* 1024)/#define DEFAULT_SEGMENT_SIZE (3 \* 1024 \* 1024)/g' {} \;
-
 # Replace "constexpr int MIN_SEGMENTS_CACHE = 5;" smaller amount
 # in tools/replay/replay.h as for some reason the argument does not appear to be working
 sed -i 's/constexpr int MIN_SEGMENTS_CACHE = 5;/constexpr int MIN_SEGMENTS_CACHE = 3;/g' tools/replay/replay.h
