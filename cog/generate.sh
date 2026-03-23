a6cfec3ac4ec49dc7a0eb3e58383f3e4344a424a
Nelson Chen <crazysim@gmail.com>
2026-03-23 07:35:20 -0700

    Make cog config generation portable on macOS

--- cog/generate.sh
+++ cog/generate.sh
@@ -4,4 +4,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Encode the setup.sh script into cog.template.yaml as base64 of ENCODED_SCRIPT and put it one directory up as cog.yml
-sed -e "s/ENCODED_SCRIPT/$(base64 -w 0 "$DIR"/../common/setup.sh)/" "$DIR"/cog.template.yaml > "$DIR"/../cog.yaml
+if base64 --help 2>/dev/null | grep -q -- "--wrap"; then
+  ENCODED_SCRIPT=$(base64 --wrap=0 "$DIR"/../common/setup.sh)
+else
+  ENCODED_SCRIPT=$(base64 < "$DIR"/../common/setup.sh | tr -d '\n')
+fi
+sed -e "s/ENCODED_SCRIPT/$ENCODED_SCRIPT/" "$DIR"/cog.template.yaml > "$DIR"/../cog.yaml
